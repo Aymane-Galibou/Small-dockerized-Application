5eb2e3deb57c2657963c7ee5e523054692f803c4
Aymane Galibou <aymanegalibou1@gmail.com>
2026-03-15 00:16:07 +0000

    finish

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -37,7 +37,8 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <h1>Hey Team Im Lgmada! 👋</h1>
-      <CurrentTime api="http://localhost/api/node/"/>
+      <CurrentTime api="/api/golang/"/>
+      <CurrentTime api="/api/node/"/>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
